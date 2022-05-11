e7bc87e84fd733eabdce402f7415581a64fb93ef
ncutixavier <ncuti60@gmail.com>
2022-05-11 11:41:20 +0200

    Integrate with backend

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,27 @@
     <v-col cols="12" sm="8" md="12">
       <v-card>
         <v-card-title>
-          <v-row justify="space-between" align="center">
-            <v-col cols="12" sm="5" md="5">Indexes</v-col>
-            <v-col cols="12" sm="6" md="6">
-              <v-text-field
-                v-model="search"
-                append-icon="mdi-magnify"
-                label="Search"
-                single-line
-                hide-details
-              ></v-text-field>
-            </v-col>
-          </v-row>
+          <v-text-field
+            v-model="search"
+            dense
+            label="Enter your query"
+            placeholder="Enter your query"
+            outlined
+            single-line
+            hide-details
+          ></v-text-field>
+          <v-btn class="ma-2" color="primary" dark :disabled="!search"
+          @click="fetchData"
+          >
+            <v-icon dark left> mdi-magnify </v-icon>
+            Search
+          </v-btn>
         </v-card-title>
         <v-data-table
           :headers="headers"
           :items="indexes"
           :search="search"
-          :loading="loading"
+          :loading="loadingStatus"
         ></v-data-table>
       </v-card>
     </v-col>
@@ -41,42 +44,28 @@ export default {
           sortable: false,
           value: "query",
         },
-        { text: "Single Index", value: "single_index" },
-        { text: "Double Index", value: "double_index" },
-        { text: "Triple Index", value: "triple_index" },
-        { text: "Multiple Index", value: "multi_index" },
+        { text: "Time", value: "time" },
       ],
-      indexes: [
-        {
-          query: "query 1",
-          single_index: "3.045ms",
-          double_index: "2.345ms",
-          triple_index: "8.005ms",
-          multi_index: "5.005ms",
-        },
-        {
-          query: "query 2",
-          single_index: "1.045ms",
-          double_index: "2.345ms",
-          triple_index: "4.005ms",
-          multi_index: "5.105ms",
-        },
-        {
-          query: "query 3",
-          single_index: "3.045ms",
-          double_index: "2.345ms",
-          triple_index: "4.005ms",
-          multi_index: "5.105ms",
-        },
-      ],
+      indexes: [],
     };
   },
+  computed: {
+    loadingStatus() {
+      return this.$store.state.loadingDoctors;
+    },
+  },
+  methods: {
+    fetchData() {
+      console.log(this.search);
+      this.$store.dispatch("doctors/fetchDoctors", this.search).then((res) => {
+        console.log("QUERY-RESP::", res);
+        this.indexes.push(...res?.data);
+      });
+    },
+  },
   mounted() {
-    this.loading = true;
-    setTimeout(() => {
-      this.loading = false;
-    }, 3000);
-    // const doctors = 
+    const doctors = this.$store.state.doctors;
+    console.log("Doctors: ", doctors);
   },
 };
 </script>
--- store/doctors.js
+++ store/doctors.js
@@ -1,34 +1,43 @@
 export const state = () => ({
-    doctors: [],
-    loadingDoctors: false,
-    errorDoctors: null,
-})
+  doctors: [],
+  loadingDoctors: false,
+  errorDoctors: null,
+});
 
 export const mutations = {
-    SET_DOCTORS(state, doctors) {
-        state.doctors = doctors
-    },
-    SET_LOADING_DOCTORS(state, loading) { 
-        state.loadingDoctors = loading
-    },
-    SET_ERROR_DOCTORS(state, error) { 
-        state.errorDoctors = error
-    }
-}
+  SET_DOCTORS(state, doctors) {
+    state.doctors = doctors;
+  },
+  SET_LOADING_DOCTORS(state, loading) {
+    state.loadingDoctors = loading;
+  },
+  SET_ERROR_DOCTORS(state, error) {
+    state.errorDoctors = error;
+  },
+};
 
 export const actions = {
-    async fetchDoctors({ commit }) { 
-        commit('SET_LOADING_DOCTORS', true)
-        try {
-            const doctors = await this.$axios.$get("/departments");
-            console.log("doctors::", doctors);
-            commit("SET_DOCTORS", doctors);
-            commit('SET_LOADING_DOCTORS', false);
-            return doctors;
-        } catch (error) {
-            commit('SET_LOADING_DOCTORS', false);
-            commit('SET_ERROR_DOCTORS', error);
-            return error;
-        }
-    }
-}
+    async fetchDoctors({ commit }, query) {
+        console.log('fetchDoctors', query);
+    return new Promise((resolve, reject) => {
+      commit("SET_LOADING_DOCTORS", true);
+      this.$axios
+        .$get(`/${query}`)
+        .then((doctors) => {
+          commit("SET_DOCTORS", doctors);
+          commit("SET_LOADING_DOCTORS", false);
+          resolve(doctors);
+        })
+        .catch((error) => {
+          commit("SET_LOADING_DOCTORS", false);
+          commit("SET_ERROR_DOCTORS", error);
+          reject(error);
+        });
+    });
+  },
+};
+
+export const getters = {
+  doctors: (state) => state.doctors,
+  loadingDoctors: (state) => state.loadingDoctors,
+};
